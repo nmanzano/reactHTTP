6fa00faf8ed86c3eccaa753d907866366081118f
Noni <noni.manzano@gmail.com>
2018-02-11 23:57:34 -0600

    fetching data on update

---
+++ src/components/FullPost/FullPost.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './FullPost.css';
+
+class FullPost extends Component {
+  state = {
+    loadPost: null
+  }
+
+  componentDidUpdate () {
+    if (this.props.id) {
+      axios.get('http://jsonplaceholder.typicode.com/posts/1' + this.props.id)
+      .then(response => {
+        // console.log(response);
+        this.setState({loadPost: response.data});
+      });
+    }
+  }
+    render () {
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        if (this.props.id) {
+          post = <p style={{textAlign:'center'}}>Loading...!</p>;
+        }
+        if (this.state.loadPost) {
+        post = (
+            <div className="FullPost">
+                <h1>{this.state.loadPost.title}</h1>
+                <p>{this.state.loadPost.body}</p>
+                <div className="Edit">
+                    <button className="Delete">Delete</button>
+                </div>
+            </div>
+
+        );
+      }
+        return post;
+    }
+}
+
+export default FullPost;
