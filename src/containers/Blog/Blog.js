a398c0e806678abeb148aeb8611ed979f5fea7cb
Noni <noni.manzano@gmail.com>
2018-02-13 00:02:15 -0600

    fixed error handle

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -36,14 +36,16 @@ class Blog extends Component {
 
     render () {
       let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
-      const posts = this.state.posts.map(post=> {
-        return <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)}
-        />;
-      })
+      if (!this.state.error) {
+         posts = this.state.posts.map(post=> {
+          return <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />;
+        })
+      }
         return (
             <div>
                 <section className="Posts">
