8cc4210e11616c07519b9b05090c10cbf3cd8d40
Noni <noni.manzano@gmail.com>
2018-02-19 00:35:10 -0600

    Rendering components for routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
+import {Route} from 'react-router-dom';
 
 class Blog extends Component {
 
@@ -16,7 +18,8 @@ class Blog extends Component {
                   </ul>
                 </nav>
               </header>
-              <Posts />
+              <Route path="/" exact component={Posts}/>
+              <Route path="/new-post" exact component={NewPost}/>
           </div>
         );
     }
