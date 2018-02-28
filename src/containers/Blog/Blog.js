a683b7cc94e9d922d63b1814252c3a016d5eec32
Noni <noni.manzano@gmail.com>
2018-02-28 00:07:24 -0600

    Redirecting request

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
-import {Route, NavLink, Switch } from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect } from 'react-router-dom';
 class Blog extends Component {
 
     render () {
@@ -33,6 +33,7 @@ class Blog extends Component {
               <Switch>
                 <Route path="/new-post" component={NewPost} />
                 <Route path="/posts" component={Posts} />
+                <Redirect from="/" to="/posts" />
               </Switch>
           </div>
         );
